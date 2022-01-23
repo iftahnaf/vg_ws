7f6195c834654643b724c57a34460f5799f90f9a
Iftach Naftaly <iftahnaf@gmail.com>
2022-01-23 20:35:06 +0200

    adding more items and arranging docker folders

--- dockers/artifacts/ros_launch_sitl.sh
+++ dockers/artifacts/ros_launch_sitl.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+yes | cp -rf mavros_posix_sitl.launch /home/vg/PX4-Autopilot/launch/
 source /opt/ros/noetic/setup.bash
 cd /home/vg/PX4-Autopilot
 source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/px4_sitl_default
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Tools/sitl_gazebo
 export PX4_SIM_SPEED_FACTOR=1
-roslaunch px4 mavros_posix_sitl.launch
+roslaunch px4 mavros_posix_sitl.launch my_model_name:=iris
