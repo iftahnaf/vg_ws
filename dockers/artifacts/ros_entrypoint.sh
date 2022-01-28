5de6feda8c6ea9a699b9a4a6a83f9f976f5f352a
Iftach Naftaly <iftahnaf@gmail.com>
2022-01-28 21:55:21 +0200

    edit ros env variables

--- dockers/artifacts/ros_entrypoint.sh
+++ dockers/artifacts/ros_entrypoint.sh
@@ -3,5 +3,7 @@ set -e
 
 source /opt/ros/noetic/setup.bash
 source /home/vg/vg_ws/devel/setup.bash
+export ROS_MASTER_URI=http://192.168.0.208:11311
+export ROS_IP=$(hostname -I | awk '{print $1}')
 
 exec "$@"
--- dockers/artifacts/ros_launch_sitl.sh
+++ dockers/artifacts/ros_launch_sitl.sh
@@ -6,5 +6,4 @@ source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/px4_sitl_default
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Tools/sitl_gazebo
 export PX4_SIM_SPEED_FACTOR=1
-export ROS_IP= $(hostname -I)
 roslaunch px4 mavros_posix_sitl.launch my_model_name:=iris_fpv_cam
